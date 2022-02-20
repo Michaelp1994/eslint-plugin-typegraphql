3967ef751db0d8b0e2c013839f411a22bc63d89b
Michael Poulgrain <michael.poulgrain@gmail.com>
2022-02-20 16:19:31 +0100

    delete dist

--- dist/lib/utils/createRule.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createRule = void 0;
-const utils_1 = require("@typescript-eslint/utils");
-exports.createRule = utils_1.ESLintUtils.RuleCreator((name) => `https://github.com/B2o5T/graphql-eslint/blob/master/docs/rules/${name}.md`);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3JlYXRlUnVsZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9saWIvdXRpbHMvY3JlYXRlUnVsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxvREFBdUQ7QUFFMUMsUUFBQSxVQUFVLEdBQUcsbUJBQVcsQ0FBQyxXQUFXLENBQUMsQ0FBQyxJQUFJLEVBQUUsRUFBRSxDQUFDLGtFQUFrRSxJQUFJLEtBQUssQ0FBQyxDQUFDIn0=
